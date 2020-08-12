149c4ba64318dc2c801118bfc377a3880ec4cd96
han-bom <livet@kakao.com>
2020-08-13 07:09:12 +0900

    design image upload

--- src/view/Design.js
+++ src/view/Design.js
@@ -5,8 +5,34 @@ import '../css/Design.css';
 class Design extends Component {
     render() {
         return (
-           <section>
+           <section className="design">
                <h2>Design</h2>
+               <div className="list">
+                   <article>
+                        <img src="/img/design/01_desktop.jpg"/>
+                   </article>
+                    <article>
+                        <img src="/img/design/02_desktop.jpg" />
+                    </article>
+                    <article>
+                        <img src="/img/design/04.jpg" />
+                    </article>
+                    <article>
+                        <img src="/img/design/05.png" />
+                    </article>
+                    <article>
+                        <img src="/img/design/06.png" />
+                    </article>
+                    <article>
+                        <img src="/img/design/07.png" />
+                    </article>
+                    <article>
+                        <img src="/img/design/08.png" />
+                    </article>
+                    <article>
+                        <img src="/img/design/09.png" />
+                    </article>
+               </div>
            </section>
         )
     }
