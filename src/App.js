2b1665213c65dcbf230204e60f0525b5362cf038
han-bom <livet@kakao.com>
2020-08-13 05:28:39 +0900

    History 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
 import Header from './view/Header';
+import History from './view/History';
 
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
 	render() { 
 		return (
 			<div className="App">
 				<Header />
+				<div className="container">
+					<History />
+				</div>
 			</div>
 		)
 	}
---
+++ src/view/History.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+
+class History extends Component {
+    render() {
+        return (
+            <section className="history">
+                <h2>History</h2>
+                <details>
+                    <summary>MBC PLUS <sub><time>2020-07</time> ~</sub></summary>
+                    <h3>주요 업무 및 역할</h3>
+                    <p>MBC PLUS의 신규사업 <a href="https://allthekshop.com">All the K-shop</a>과 <a href="https://sportsmania.co.kr">sportsmania</a> 총괄 개발자</p>
+                    <h4>사용언어</h4>
+                    <ul>
+                        <li>html</li>
+                        <li>css</li>
+                        <li>js</li>
+                        <li>php</li>
+                        <li>template_(php 템플릿 엔진)</li>
+                    </ul>
+                    <h3>업무 성과</h3>
+                    <ul>
+                        <li>SNS 공유하기 기능 추가</li>
+                        <li>
+                            상품권 기능 개발
+                            <ul>
+                                <li>상품권 csv 업로드</li>
+                                <li>상품권 구매시 자동발급</li>
+                                <li>재고 관리 시스템</li>
+                            </ul>
+                        </li>
+                        <li>글로벌 사이트 구현</li>
+                        <li>해외결제 서비스 연동</li>
+                        <li>동영상 관리 페이지 구현</li>
+                        <li>동영상 관련 추천상품 개발</li>
+                        <li>접속환경(PC/Mobile) 리다이렉트</li>
+                        <li>간단한 퍼블리싱 업무</li>
+                        <li>유지보수</li>
+                    </ul>
+                </details>
+                <details>
+                    <summary>(주)와우소프트 <sub><time>2018-09</time> ~ <time>2020-01</time></sub></summary>
+                    <h3>주요 업무 및 역할</h3>
+                    <p>삼성전자 스마트 냉장고(쉐프 컬렉션)의 OS 패밀리 허브 어플리케이션 개발</p>
+                    <h4>사용언어</h4>
+                    <ul>
+                        <li>html</li>
+                        <li>css</li>
+                        <li>js</li>
+                        <li>node js (express js, angular7)</li>
+                        <li>mysql</li>
+                        <li>sqlite</li>
+                        <li>C/C++</li>
+                        <li>enlightenment(C/C++ GUI Library)</li>
+                    </ul>
+                    <h3>업무 성과</h3>
+                    <ul>
+                        <li>
+                            FH4.0 어플리케이션[식단 플래너, 쇼핑 리스트, 날씨, 이마트 몰, Deals(미국 식품 쇼핑), WoolWorths(호주 식
+                            품 쇼핑), Pandora(미국 음악 스트리밍)] 유지보수 및 개발
+                            <ul>
+                                <li>메모리 누수 수정</li>
+                                <li>Curl 통신 충돌 이슈</li>
+                                <li>이벤트 리스너 오작동 이슈 수정</li>
+                                <li>Bixby 2.0 기능 개발</li>
+                                <li>모바일 앱과 실시간 정보 교환 백그라운드 어플리케이션 개발</li>
+                                <li>Cloud 데이터 관리 백그라운드 어플리케이션 개발</li>
+                                <li>식단플래너 리뉴얼</li>
+                                <li>Pandora, 날씨, Melon 신규 위젯 개발</li>
+                                <li>유지보수</li>
+                            </ul>
+                        </li>                        
+                        <li>
+                            FH4.0 멜론 어플리케이션 개발
+                            <ul>
+                                <li>사운드 재생, 로그인 정보 관리 백그라운드 어플리케이션 개발</li>
+                                <li>어플리케이션용 DB 구현</li>
+                                <li>화면보호기 플레이어 위젯 개발</li>
+                                <li>홈스크린 플레이어 위젯 개발</li>
+                                <li>간편화면 플레이어 위젯 개발</li>
+                                <li>태스크바 플레이어 위젯 개발</li>
+                            </ul>
+                        </li>
+                        <li>출력물 보안 프로그램 QA</li>
+                    </ul>
+                </details>
+                <details>
+                    <summary>(주)웰게이트 <sub><time>2017-08</time> ~ <time>2018-09</time></sub></summary>
+                    <h3>주요 업무 및 역할</h3>
+                    <p>삼성전자 스마트 냉장고(쉐프 컬렉션)의 OS 패밀리 허브 어플리케이션 개발<br/>내부 프로젝트</p>
+                    <h4>사용언어</h4>
+                    <ul>
+                        <li>html</li>
+                        <li>css</li>
+                        <li>js</li>
+                        <li>node js (express js, angular7)</li>
+                        <li>mysql</li>
+                        <li>sqlite</li>
+                        <li>C/C++</li>
+                        <li>enlightenment(C/C++ GUI Library)</li>
+                    </ul>
+                    <h3>업무 성과</h3>
+                    <ul>
+                        <li>
+                            FH3.0 어플리케이션 [식단 플래너, 쇼핑 리스트, 날씨, 이마트 몰, Deals(미국 식품 쇼핑), WoolWorths(호주 식
+                            품 쇼핑), Pandora(미국 음악 스트리밍)]
+                            <ul>
+                                <li>Bixby 기능 개발</li>
+                                <li>식단 플래너 앱 개발</li>
+                                <li>쇼핑 리스트 앱 개발</li>
+                                <li>날씨 앱 개발</li>
+                                <li>날씨 화면보호기 개발</li>
+                                <li>날씨 홈스크린 위젯 개발</li>
+                                <li>이마트 몰 홈스크린 위젯 개발</li>
+                                <li>Deals(미국 식품 쇼핑) 앱 개발</li>
+                                <li>Deals(미국 식품 쇼핑) 위젯 개발</li>
+                                <li>WoolWorths(호주 식품 쇼핑) 앱 개발</li>
+                                <li>Pandora(미국 음악 스트리밍) 앱 개발</li>
+                                <li>Pandora(미국 음악 스트리밍) 화면 보호기 위젯 개발</li>
+                                <li>Pandora(미국 음악 스트리밍) 홈스크린 위젯 개발</li>
+                                <li>Pandora(미국 음악 스트리밍) 태스크바 위젯 개발</li>
+                                <li>Pandora(미국 음악 스트리밍) 백그라운드 어플리케이션 개발</li>
+                                <li>유지보수</li>
+                            </ul>
+                        </li>
+                        <li>갤럭시 Bixby voice id(음성으로 사용자를 여부 판별 프로세스) 개발</li>
+                        <li>미국 배관공 회사(plumbplus.net)의 전산 시스템 개발</li>
+                        <li>회사 내부 Git 서버 및 외부 접속용 페이지 개발</li>
+                    </ul>
+                </details>
+            </section>
+        )
+    }
+}
+
+export default History;
