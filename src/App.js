bcd1adf6642c61ed69de69af037f7dc572f2a474
han-bom <livet@kakao.com>
2020-08-13 00:59:26 +0900

    header 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+
+import Header from './view/Header';
+
 import './App.css';
 
 class App extends Component {
 	render() { 
 		return (
 			<div className="App">
+				<Header />
 			</div>
 		)
 	}
