7f8a5e298724f6849cbb3a20fe1d740f8d6eb95c
han-bom <livet@kakao.com>
2020-08-13 11:58:00 +0900

    develop 누락내용기재

--- src/view/Develop.js
+++ src/view/Develop.js
@@ -9,129 +9,156 @@ class Develop extends Component {
                 <h2>Develop</h2>
                 <div className="list">
                     <article>
-                        <h3>햄버거 버튼 애니메이션<sub><time>2015-12</time></sub></h3>
-                        <p>css만으로 만든 햄버거 버튼 애니메이션입니다. 좌측상단 햄버거 버튼 클릭시 애니메이션 되며 메뉴가 노출됩니다.</p>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                        </ul>
+                        <a href="/dump/wallplus.html">
+                            <h3>Wall Plus<sub><time>2020-06</time></sub></h3>
+                            <p>Wall Paper Engine을 따라한 개인 프로젝트입니다.</p>
+                            <ul>
+                                <li>html</li>
+                                <li>css</li>
+                                <li>js</li>
+                                <li>react</li>
+                                <li>webpack</li>
+                                <li>babel</li>
+                                <li>electron</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>canvas 그래프 애니메이션<sub><time>2016-02</time></sub></h3>
-                        <p>간단한 도넛 모양 그래프 애니메이셥입니다</p>
-                        <ul>
-                            <li>js</li>
-                        </ul>
+                        <a href="/dump/1.html">
+                            <h3>햄버거 버튼 애니메이션<sub><time>2015-12</time></sub></h3>
+                            <p>css만으로 만든 햄버거 버튼 애니메이션입니다. 좌측상단 햄버거 버튼 클릭시 애니메이션 되며 메뉴가 노출됩니다.</p>
+                            <ul>
+                                <li>html</li>
+                                <li>css</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>1010! 게임<sub><time>2016-05</time></sub></h3>
-                        <p>모바일 1010!을 따라만든 웹게임입니다.</p>
-                        <ul>
-                            <li>js</li>
-                            <li>jquery</li>
-                        </ul>
+                        <a href="/dump/2.html">
+                            <h3>canvas 그래프 애니메이션<sub><time>2016-02</time></sub></h3>
+                            <p>간단한 도넛 모양 그래프 애니메이셥입니다</p>
+                            <ul>
+                                <li>js</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>1010! 게임<sub><time>2016-05</time></sub></h3>
-                        <p>모바일 1010!을 따라만든 웹게임입니다.</p>
-                        <ul>
-                            <li>js</li>
-                            <li>jquery</li>
-                        </ul>
+                        <a href="/dump/10">
+                            <h3>1010! 게임<sub><time>2016-05</time></sub></h3>
+                            <p>모바일 1010!을 따라만든 웹게임입니다.</p>
+                            <ul>
+                                <li>js</li>
+                                <li>jquery</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>꽃점<sub><time>2020-07</time></sub></h3>
-                        <p>한다, 안한다 입력란을 각각 채우고 점괘보기를 누르면 꽃잎의 수가 랜덤인 꽃이 생성되며, 꽃을 클릭시 꽃잎이 한장씩 떨어집니다.</p>
-                        <ul>
-                            <li>js</li>
-                        </ul>
+                        <a href="/dump/20">
+                            <h3>꽃점<sub><time>2020-07</time></sub></h3>
+                            <p>한다, 안한다 입력란을 각각 채우고 점괘보기를 누르면 꽃잎의 수가 랜덤인 꽃이 생성되며, 꽃을 클릭시 꽃잎이 한장씩 떨어집니다.</p>
+                            <ul>
+                                <li>js</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>퍼블리싱 연습사이트1<sub><time>2016-06</time></sub></h3>
-                        <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                        </ul>
+                        <a href="/dump/30">
+                            <h3>퍼블리싱 연습사이트1<sub><time>2016-06</time></sub></h3>
+                            <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
+                            <ul>
+                                <li>html</li>
+                                <li>css</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>퍼블리싱 연습사이트2<sub><time>2016-06</time></sub></h3>
-                        <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                        </ul>
+                        <a href="/dump/40">
+                            <h3>퍼블리싱 연습사이트2<sub><time>2016-06</time></sub></h3>
+                            <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
+                            <ul>
+                                <li>html</li>
+                                <li>css</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>퍼블리싱 연습사이트3<sub><time>2016-05</time></sub></h3>
-                        <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                        </ul>
+                        <a href="/dump/60">
+                            <h3>퍼블리싱 연습사이트3<sub><time>2016-05</time></sub></h3>
+                            <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
+                            <ul>
+                                <li>html</li>
+                                <li>css</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>퍼블리싱 연습사이트4<sub><time>2016-07</time></sub></h3>
-                        <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                        </ul>
+                        <a href="/dump/70/1">
+                            <h3>퍼블리싱 연습사이트4<sub><time>2016-07</time></sub></h3>
+                            <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
+                            <ul>
+                                <li>html</li>
+                                <li>css</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>이미지 카드 리스트<sub><time>2016-07</time></sub></h3>
-                        <p>카드에 이미지를 여러장 올릴수 있습니다.</p>
-                        <ul>
-                            <li>js</li>
-                            <li>jquery</li>
-                        </ul>
+                        <a href="/dump/70/2">
+                            <h3>이미지 카드 리스트<sub><time>2016-07</time></sub></h3>
+                            <p>카드에 이미지를 여러장 올릴수 있습니다.</p>
+                            <ul>
+                                <li>js</li>
+                                <li>jquery</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>퍼블리싱 연습사이트5<sub><time>2016-07</time></sub></h3>
-                        <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                        </ul>
+                        <a href="/dump/80">
+                            <h3>퍼블리싱 연습사이트6<sub><time>2015-11</time></sub></h3>
+                            <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
+                            <ul>
+                                <li>html</li>
+                                <li>css</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>퍼블리싱 연습사이트6<sub><time>2015-11</time></sub></h3>
-                        <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                        </ul>
+                        <a href="/dump/90">
+                            <h3>퍼블리싱 연습사이트7<sub><time>2015-11</time></sub></h3>
+                            <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
+                            <ul>
+                                <li>html</li>
+                                <li>css</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>퍼블리싱 연습사이트7<sub><time>2015-11</time></sub></h3>
-                        <p>간단한 디자인으로 만든 퍼블리싱 연습사이트입니다.</p>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                        </ul>
+                        <a href="/dump/100">
+                            <h3>엇박자 슬라이더<sub><time>2016-03</time></sub></h3>
+                            <p>간단한 슬라이더입니다.</p>
+                            <ul>
+                                <li>js</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>엇박자 슬라이더<sub><time>2016-03</time></sub></h3>
-                        <p>간단한 슬라이더입니다.</p>
-                        <ul>
-                            <li>js</li>
-                        </ul>
+                        <a href="/dump/110">
+                            <h3>svg 애니메이션<sub><time>2016-04</time></sub></h3>
+                            <p>svg에 마우스 오버 애니메이션이 있습니다.</p>
+                            <ul>
+                                <li>css</li>
+                            </ul>
+                        </a>
                     </article>
                     <article>
-                        <h3>svg 애니메이션<sub><time>2016-04</time></sub></h3>
-                        <p>svg에 마우스 오버 애니메이션이 있습니다.</p>
-                        <ul>
-                            <li>css</li>
-                        </ul>
-                    </article>
-                    <article>
-                        <h3>Animated Login Form<sub><time>2016-01</time></sub></h3>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                            <li>js</li>
-                            <li>jquery</li>
-                        </ul>
+                        <a href="/dump/120">
+                            <h3>Animated Login Form<sub><time>2016-01</time></sub></h3>
+                            <ul>
+                                <li>html</li>
+                                <li>css</li>
+                                <li>js</li>
+                                <li>jquery</li>
+                            </ul>
+                        </a>
                     </article>
                 </div>
             </section>
